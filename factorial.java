9e297d99def66bc11765eb17eb9f9dfffbfb33dc
Ammu19 <80750615+Ammu19@users.noreply.github.com>
2021-03-16 15:03:41 +0530

    One number added

--- factorial.java
+++ factorial.java
@@ -1,6 +1,10 @@
 import java.util.*;
 public class Ammu{
   public static void main{
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the 1st number");
+    int n=sc.nextInt();
+    
     
   }
 }
